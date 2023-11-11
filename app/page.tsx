3e671499c8ed166ca19e5f306664160850bfd1fe
OriginalByteMe <noahrijkaard@gmail.com>
2023-11-12 02:38:54 +0800

    Welcome page design

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-
+import { Welcome } from '@/components/Welcome/Welcome';
 export default function HomePage() {
   return (
     <>
       <Welcome />
-      <ColorSchemeToggle />
     </>
   );
 }
--- components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,15 +1,41 @@
 'use client';
 
-import { Button, Group, useMantineColorScheme } from '@mantine/core';
+import { Button, useMantineColorScheme, ThemeIcon  } from '@mantine/core';
+import { useState } from 'react';
+import { FaSun, FaMoon, FaSync } from 'react-icons/fa';
 
 export function ColorSchemeToggle() {
-  const { setColorScheme } = useMantineColorScheme();
+  const [scheme, setScheme] = useState('auto');
+  const { setColorScheme, colorScheme } = useMantineColorScheme();
+
+  const handleClick = () => {
+    if (scheme === 'auto') {
+      setScheme('light');
+      setColorScheme('light');
+    } else if (scheme === 'light') {
+      setScheme('dark');
+      setColorScheme('dark');
+    } else {
+      setScheme('auto');
+      setColorScheme('auto');
+    }
+  };
+
+  const getIcon = () => {
+    if (scheme === 'auto') {
+      return <FaSync />;
+    } else if (scheme === 'light') {
+      return <FaSun />;
+    } else {
+      return <FaMoon />;
+    }
+  };
 
   return (
-    <Group justify="center" mt="xl">
-      <Button onClick={() => setColorScheme('light')}>Light</Button>
-      <Button onClick={() => setColorScheme('dark')}>Dark</Button>
-      <Button onClick={() => setColorScheme('auto')}>Auto</Button>
-    </Group>
+    <Button onClick={handleClick} variant="subtle">
+      <ThemeIcon color={colorScheme === 'light' ? 'black' : 'lilac'}  radius="xl">
+        {getIcon()}
+      </ThemeIcon>
+    </Button>
   );
 }
