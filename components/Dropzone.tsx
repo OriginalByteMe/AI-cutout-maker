4b4bf26322ddd30c06ce6ece63b5232f6ab6c6bc
OriginalByteMe <noahrijkaard@gmail.com>
2023-11-16 00:11:03 +0800

    Update upload URL in Dropzone component

--- components/Dropzone.tsx
+++ components/Dropzone.tsx
@@ -8,7 +8,7 @@ type UploadStatus = "preparing" | "aborted" | "uploading" | "success" | "error";
 
 export default function Dropzone() {
   const [files, setFiles] = useState<ExtFile[]>([]);
-  const uploadUrl = process.env.NEXT_PUBLIC_UPLOAD_URL;
+  const uploadUrl = process.env.NEXT_PUBLIC_S3_API + "/upload-image";
   const [dropOccurred, setDropOccurred] = useState<boolean>(false);
   const [uploadSuccessful, setUploadSuccessful] = useState(false);
 
@@ -49,7 +49,7 @@ export default function Dropzone() {
       },
     };
     try {
-      const response = await axios.post(uploadUrl, formData, config);
+      const response = await axios.post(uploadUrl as string, formData, config);
       setFiles(files.map((f) => {
         if (f.id === file.id) {
           setUploadSuccessful(true);
@@ -78,42 +78,46 @@ export default function Dropzone() {
 
   return (
     <>
-      <FilesUIDropzone
-        onChange={updateFiles}
-        value={files}
-        accept={"image/*"}
-        maxFileSize={28 * 1024*1024}
-        maxFiles={1}
-      >
-        {files.map((file: ExtFile) => (
-          <FileMosaic
-            key={file.id}
-            {...file}
-            onDelete={removeFile}
-            info
-            preview
-            uploadStatus={file.uploadStatus}
-          />
-        ))}
-      </FilesUIDropzone>
-      {files.length > 0 && (
-        <>
-          <div className="flex items-center justify-center">
-            {!uploadSuccessful ? (
-              <Button onClick={() => uploadFile(files[0])}>Upload</Button>
-            ) : (
-              <Link href="/cutouts">
-                <Button>Next</Button>
-              </Link>
-            )}
-          </div>
-          {uploadSuccessful && (
-            <Notification title="Upload successful" color="green">
-              Your image has been uploaded successfully. Click the button below to continue.
-            </Notification>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="w-full max-w-lg">
+          <FilesUIDropzone
+            onChange={updateFiles}
+            value={files}
+            accept={"image/*"}
+            maxFileSize={28 * 1024*1024}
+            maxFiles={1}
+          >
+            {files.map((file: ExtFile) => (
+              <FileMosaic
+                key={file.id}
+                {...file}
+                onDelete={removeFile}
+                info
+                preview
+                uploadStatus={file.uploadStatus}
+              />
+            ))}
+          </FilesUIDropzone>
+          {files.length > 0 && (
+            <>
+              <div className="flex items-center justify-center mt-4">
+                {!uploadSuccessful ? (
+                  <Button onClick={() => uploadFile(files[0])}>Upload</Button>
+                ) : (
+                  <Link href="/cutouts">
+                    <Button>Next</Button>
+                  </Link>
+                )}
+              </div>
+              {uploadSuccessful && (
+                <Notification title="Upload successful" color="green">
+                  Your image has been uploaded successfully. Click the button below to continue.
+                </Notification>
+              )}
+            </>
           )}
-        </>
-      )}
+        </div>
+      </div>
     </>
   );
 }
