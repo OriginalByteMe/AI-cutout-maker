a8c4d1bfdfc93c2311717d3fd44ef8a79e7ce24d
OriginalByteMe <noahrijkaard@gmail.com>
2023-11-17 19:07:28 +0800

    Refactor upload page to use Dropzone component and add useWarmup hook

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,10 +1,11 @@
 'use client';
-import { useState } from 'react';
-import { Notification } from '@mantine/core';
-
 import Dropzone from '@/components/Dropzone';
-export default function UploadPage() {
+import useWarmup from '@/hooks/useWarmup';
+import { Notification } from '@mantine/core';
+import { useState } from 'react';
 
+export default function UploadPage() {
+  useWarmup();
   return (
       <>
         <Dropzone />
