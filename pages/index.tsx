6970d42a36cf8cce7949053806f87f9b57445ce8
OriginalByteMe <noahrijkaard@gmail.com>
2023-11-17 19:07:05 +0800

    Add useWarmup hook to fetch warmup data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import { Welcome } from '@/components/Welcome/Welcome';
+import useWarmup from '@/hooks/useWarmup';
+
 export default function HomePage() {
+  useWarmup();
   return (
     <>
       <Welcome />
