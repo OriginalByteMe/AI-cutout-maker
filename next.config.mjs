1b880b062345f7c0fa77d2765855ae3105170038
Noah Rijkaard <noah.rijkaard@merchantspring.com.au>
2025-12-10 17:29:49 +0800

    enable cached components

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const withBundleAnalyzer = bundleAnalyzer({
 
 export default withBundleAnalyzer({
   reactStrictMode: false,
+  cacheComponents: true,
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
